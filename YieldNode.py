662b0b08a4cbe54aaffa94a7689fd0eb66958b34
Jeadie <jack.eadie0@gmail.com>
2018-03-28 13:44:27 +1000

    initial setup and init for yield lattices

--- YieldNode.py
+++ YieldNode.py
@@ -1,35 +1,36 @@
 class YieldNode(object):
 
-	def __init__(self, time, p, y): 
+	def __init__(self, time, p, y, prev_nodes = None, next_nodes = None): 
 		self.prev_up = None
 		self.prev_down = None
 		self.next_up = None
 		self.next_down = None
 		self.time = time
-		self.p = p
+		self._p = p
 		self.y = y
+		
+		if((prev_nodes is None) | (next_nodes is None)): 
+			return 
 
-	def __init__(self, time, p, y, prev_nodes, next_nodes): 
-		__init__(self, time, p, y)
-		if(length(prev_nodes) ==2): 
+		if(len(prev_nodes) ==2): 
 			self.prev_up = prev_nodes[0]
 			self.prev_down = prev_nodes[1]
-		if(length(next_nodes) ==2): 
+		if(len(next_nodes) ==2): 
 			self.next_up = next_nodes[0]
 			self.next_down = next_nodes[1]
 
 	@property
 	def p(self): 
-		return self.p
+		return self._p
 
 	@p.setter
 	def p(self, value): 
-		if value >=0 & value <=1: 
-			self.p = value
+		if (value >=0) & (value <=1): 
+			self._p = value
 	
 
 	def __str__(self): 
-		return "YieldNode: time={0}, yield={1}.".format(self.time, self.y)
+		return "[YieldNode: time={0}, yield={1}]".format(self.time, self.y)
 
 class ForwardYieldLattice(object): 
 	
@@ -47,11 +48,11 @@ class ForwardYieldLattice(object):
 
 	def init_nodes(self): 
 		#construct root node manually
-		root_node = YieldNode(0, 1, self.y0)
-
+		root_node = YieldNode(0, 0.5, self.y0)
+		self.NodeLayers[0].append(root_node)
 		# for each layer create new nodes and link back to prior nodes
-		for	layer, time in zip(self.NodeLayers[1:], range(1, self.durations)): 
-			for i in range(2, time +1): 
+		for	layer, time in zip(self.NodeLayers[1:], range(1, self.duration)): 
+			for i in range(0, time +1): 
 				prev_node_down = self.get_node(time -1, i)
 				prev_node_up =  self.get_node(time -1, i-1)
 				node = None
@@ -59,13 +60,13 @@ class ForwardYieldLattice(object):
 				if prev_node_down is not None: 
 					y = prev_node_down.y
 					p = prev_node_down.p
-					node = YieldNode(i, time, self.p(time-1, y, p), self.y_u(time-1, y, p), [prev_node_up, prev_node_down], [None, None])
+					node = YieldNode(time, self.p(time-1, y, p), self.y_u(time-1, y, p), prev_nodes = [prev_node_up, prev_node_down], next_nodes = [None, None])
 
 				# if node is final node in layer
 				else: 
 					y = prev_node_up.y
 					p = prev_node_up.p
-					node = YieldNode(i, time, self.p(time-1,y,p), self.y_d(time-1, y, p), [prev_node_up, prev_node_down], [None, None])
+					node = YieldNode(time, self.p(time-1,y,p), self.y_d(time-1, y, p), [prev_node_up, prev_node_down], [None, None])
 				self.NodeLayers[time].append(node)
 
 	def get_node(self, t, i): 
@@ -78,11 +79,16 @@ class ForwardYieldLattice(object):
 		# check if node exists at given time and index
 		if(t < 0 | (i < 0 | i > t)): 
 			return None
-		return self.NodeLayers()[t][i]
-
+		try:	
+			return self.NodeLayers[t][i]
+		except: 
+			return None
 	def __str__(self): 
-		return "\n".join([str(i) for i in layer for layer in self.NodeLayers])
+		layers = [" ".join([str(i) for i in layer]) for layer in self.NodeLayers]
+		return "\n".join(layers)
 
-if '__name__' == '__main__': 
+
+if __name__ == '__main__': 
 	a = ForwardYieldLattice(5, 0.5, 0.03, 0.01); 
 	print(a)
+	print("Done")
