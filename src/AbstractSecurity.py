acd15ca945ef94bc70b17950e0c8d3dec445deee
Jeadie <jack.eadie0@gmail.com>
2018-03-30 20:52:39 +1000

    django template and binom app setup

---
+++ src/AbstractSecurity.py
@@ -0,0 +1,8 @@
+
+class AbstractSecurity(object):
+    """
+    Abstract interface for a variety of securities including: Single and Multiple Cashflows.
+      Bonds, Simple & Compound Interest Loans, Annuities, perpetuities
+    """
+
+    def __init__(self, t0, duration, cashflows):
